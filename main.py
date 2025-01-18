2a73b93bffe1e4ee6260faa5aa1e4abb488b0392
Dhiraj Das <dhiraj.das.05@gmail.com>
2025-01-19 00:37:17 +0530

    updated flight post api

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from typing import Union
 from fast_flights import FlightData, Passengers, Result, get_flights
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Flight(BaseModel):
+    origin: str
+    destination: str
+    start_date: str
 
 @app.get("/")
 async def read_root():
@@ -13,7 +18,7 @@ async def read_root():
 async def search_flight():
     result: Result = get_flights(
         flight_data=[
-            FlightData(date="2025-20-03", from_airport="GAU", to_airport="DEL")
+            FlightData(date="2025-03-03", from_airport="GAU", to_airport="DEL")
         ],
         trip="one-way",
         seat="economy",
@@ -21,6 +26,25 @@ async def search_flight():
         fetch_mode="fallback",
     )
     return result
+
+@app.post("/flight/")
+async def create_flight(flight: Flight):
+    result: Result = get_flights(
+        flight_data=[
+            FlightData(date=flight.start_date, from_airport=flight.origin, to_airport=flight.destination)
+        ],
+        trip="one-way",
+        seat="economy",
+        passengers=Passengers(adults=1, children=0, infants_in_seat=0, infants_on_lap=0),
+        fetch_mode="fallback",
+    )
+
+    response = {
+        "price_type": result.current_price,
+        "flights": result.flights[0:3]
+    }
+
+    return response
 
 
 @app.get("/items/{item_id}")
