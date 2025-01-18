64cbad1dc858a4b12f7e9293c03ba63ebe590d18
Dhiraj Das <dhiraj.das.05@gmail.com>
2025-01-18 16:06:16 +0530

    modified code

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Union
-
+from fast_flights import FlightData, Passengers, Result, get_flights
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -8,6 +8,19 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
+@app.get("/flights/")
+async def search_flight():
+    result: Result = get_flights(
+        flight_data=[
+            FlightData(date="2025-20-03", from_airport="GAU", to_airport="DEL")
+        ],
+        trip="one-way",
+        seat="economy",
+        passengers=Passengers(adults=1, children=0, infants_in_seat=0, infants_on_lap=0),
+        fetch_mode="fallback",
+    )
+    return result
 
 
 @app.get("/items/{item_id}")
